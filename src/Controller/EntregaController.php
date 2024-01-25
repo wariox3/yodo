1ec46a9aad7cf4e609c184871c1d70152ae77fe4
MarioA <maestradaz3@gmail.com>
2024-01-25 10:13:32 -0500

    Modelo entregas

--- src/Controller/EntregaController.php
+++ src/Controller/EntregaController.php
@@ -48,4 +48,53 @@ class EntregaController extends AbstractFOSRestController
         }
     }
 
+    #[Route('/api/entrega/detalle', name: 'api_entrega_detalle')]
+    public function detalle(Request $request, EntityManagerInterface $em) {
+        $raw = json_decode($request->getContent(), true);
+        $codigoEntrega = $raw['codigoEntrega']?? null;
+        if($codigoEntrega) {
+            $arrRespuesta = $em->getRepository(Entrega::class)->detalle($codigoEntrega);
+            if(!$arrRespuesta['error']) {
+                return $this->view($arrRespuesta['respuesta'], 200);
+            } else {
+                return $this->view(['mensaje' => $arrRespuesta['errorMensaje']], 400);
+            }
+        } else {
+            return $this->view(['mensaje' => 'Faltan datos para el consumo de la API'], 400);
+        }
+    }
+
+    #[Route('/api/entrega/pendiente', name: 'api_entrega_pendiente')]
+    public function pendiente(Request $request, EntityManagerInterface $em) {
+        $raw = json_decode($request->getContent(), true);
+        $codigoPanal = $raw['codigoPanal']?? null;
+        $celda = $raw['celda']?? null;
+        if($codigoPanal) {
+            $arrRespuesta = $em->getRepository(Entrega::class)->pendiente($codigoPanal, $celda);
+            if(!$arrRespuesta['error']) {
+                return $this->view($arrRespuesta['respuesta'], 200);
+            } else {
+                return $this->view(['mensaje' => $arrRespuesta['errorMensaje']], 400);
+            }
+        } else {
+            return $this->view(['mensaje' => 'Faltan datos para el consumo de la API'], 400);
+        }
+    }
+
+    #[Route('/api/entrega/entrega', name: 'api_entrega_entrega')]
+    public function entrega(Request $request, EntityManagerInterface $em) {
+        $raw = json_decode($request->getContent(), true);
+        $codigoEntrega = $raw['codigoEntrega']?? null;
+        $codigoUsuario = $raw['codigoUsuario']?? null;
+        if($codigoEntrega && $codigoUsuario) {
+            $arrRespuesta = $em->getRepository(Entrega::class)->entrega($raw);
+            if(!$arrRespuesta['error']) {
+                return $this->view($arrRespuesta['respuesta'], 200);
+            } else {
+                return $this->view(['mensaje' => $arrRespuesta['errorMensaje']], 400);
+            }
+        } else {
+            return $this->view(['mensaje' => 'Faltan datos para el consumo de la API'], 400);
+        }
+    }
 }
--- src/Repository/EntregaRepository.php
+++ src/Repository/EntregaRepository.php
@@ -7,6 +7,7 @@ use App\Entity\CeldaUsuario;
 use App\Entity\Entrega;
 use App\Entity\EntregaTipo;
 use App\Entity\Panal;
+use App\Entity\Usuario;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -86,4 +87,97 @@ class EntregaRepository extends ServiceEntityRepository
             ];
         }
     }
+
+    public function detalle($codigoEntrega)
+    {
+        $em = $this->getEntityManager();
+        $queryBuilder = $em->createQueryBuilder()->from(Entrega::class, 'e')
+            ->select('e.id')
+            ->addSelect('e.fechaIngreso')
+            ->addSelect('e.entregaTipoId')
+            ->addSelect('e.descripcion')
+            ->addSelect('c.celda')
+            ->addSelect('et.nombre as entregaTipoNombre')
+            ->leftJoin('e.celda', 'c')
+            ->leftJoin('e.entregaTipo', 'et')
+            ->where("e.id = {$codigoEntrega}");
+        $arEntrega = $queryBuilder->getQuery()->getOneOrNullResult();
+        return [
+            'error' => false,
+            'respuesta' => [
+                'entrega' => $arEntrega
+            ]
+        ];
+    }
+
+    public function pendiente($codigoPanal, $celda)
+    {
+        $em = $this->getEntityManager();
+        $queryBuilder = $em->createQueryBuilder()->from(Entrega::class, 'e')
+            ->select('e.id')
+            ->addSelect('e.fechaIngreso')
+            ->addSelect('e.entregaTipoId')
+            ->addSelect('e.descripcion')
+            ->addSelect('c.celda')
+            ->leftJoin('e.celda', 'c')
+            ->where("c.panalId = {$codigoPanal}")
+            ->andWhere("e.estadoEntregado = false")
+            ->addOrderBy("e.fechaIngreso", "DESC");
+        if($celda) {
+            $queryBuilder->andWhere("c.celda = '{$celda}'");
+        }
+        $arEntregas = $queryBuilder->getQuery()->getResult();
+        return [
+            'error' => false,
+            'respuesta' => [
+                'entregas' => $arEntregas
+            ]
+        ];
+    }
+
+    public function entrega($raw)
+    {
+        $em = $this->getEntityManager();
+        $codigoEntrega = $raw['codigoEntrega']?? null;
+        $codigoUsuario = $raw['codigoUsuario']?? null;
+        $arrImagen = $raw['imagenBase64']??[];
+        $arEntrega = $em->getRepository(Entrega::class)->find($codigoEntrega);
+        if($arEntrega) {
+            if($arEntrega->isEstadoEntregado() == false) {
+                $arUsuario = $em->getRepository(Usuario::class)->find($codigoUsuario);
+                if($arUsuario) {
+                    $arEntrega->setEstadoEntregado(true);
+                    $arEntrega->setFechaEntrega(new \DateTime('now'));
+                    if($arrImagen) {
+                        //$archivo = $this->space->subir('entrega', $imagen);
+                        //$arEntrega->setUrlImagen($archivo['url']);
+                    }
+                    $em->persist($arEntrega);
+                    $em->flush();
+                    return [
+                        'error' => false,
+                        'respuesta' => [
+                            'mensaje' => 'La entrega fue entregada con exito'
+                        ]
+                    ];
+                } else {
+                    return [
+                        'error' => true,
+                        'errorMensaje' => "No existe el usuario"
+                    ];
+                }
+            } else {
+                return [
+                    'error' => true,
+                    'errorMensaje' => "La entrega ya fue entregada al cliente"
+                ];
+            }
+        } else {
+            return [
+                'error' => true,
+                'errorMensaje' => "No existe la entrega"
+            ];
+        }
+    }
+
 }
