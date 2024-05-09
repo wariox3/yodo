78e580e4743b0a4a9b7b4549f9a2c654cd6ad7b4
sebas957 <baxtian957@hotmail.com>
2024-05-09 09:51:50 -0500

    ajustes pruebas

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
